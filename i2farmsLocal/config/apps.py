10850254472eb554fb166045c3a3d32843382242
rathod.gauravvinod@gmail.com <rathod.gauravvinod@gmail.com>
2022-03-10 18:42:23 +0530

    all apps created

--- i2farmsLocal/config/apps.py
+++ i2farmsLocal/config/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ThConfig(AppConfig):
+class ConfigConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'th'
+    name = 'config'
---
+++ i2farmsLocal/hum/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HumConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'hum'
